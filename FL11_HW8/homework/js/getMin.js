d00631b9caffb3f623f231575266543fe4b426f9
Oleksandr Belchitskyi <oleksandr.belchitskyi@gmail.com>
2019-07-12 18:30:53 +0300

    13. Task #8. Change getMin.js according to the             task description #8
    	    Change application for subtask #3, without use Math.min()
    	        FL11_HW8/homework/js/getMin.js.

--- FL11_HW8/homework/js/getMin.js
+++ FL11_HW8/homework/js/getMin.js
@@ -1,5 +1,12 @@
 function getMin() {
-    return Math.min(...arguments);
+    const arr = [...arguments];
+    let min = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+    }
+    return min;
 }
 
 console.log(getMin(45,0,5,1,-1,8));
