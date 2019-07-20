6c8c0b408cbbf50b783725bf08590561cb94daf0
Alexander BELCHITSKIY <alexander.belchitskiy@1000geeks.com>
2019-07-21 00:30:57 +0300

    16. Task #10. Change fighters_game.js according to the 	     task description #10 with use Scoped WeakMap
    	     Create application for task #10
    	         FL11_HW10/homework/fighters_game.js

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -1,155 +1,109 @@
-class Fighter {
-  #name = '';
-  #damage = 0;
-  #hp = 0;
-  #agility = 0;
-  #wins = 0;
-  #losses = 0;
+const hundred = 100;
 
-  constructor (_name = "No name", _damage, _hp, _agility) {
-    this.#name = _name;
-    this.#damage = _damage;
-    this.#hp = _hp;
-    this.#agility = _agility;
-    console.log(`    ${this.#name} : damage - ${this.#damage}, hp - ${this.#hp}, agility - ${this.#agility}`);
-  }
+const Fighter = (function () {
+  const privateData = new WeakMap();
 
-  getName () {
-    return this.#name;
-  }
-
-  getDamage () {
-    return this.#damage;
-  }
-
-  getHealth () {
-    return this.#hp;
-  }
-
-  getAgility () {
-    return this.#agility;
-  }
-
-  attack(_fighter) {
-    let hit = Math.round(Math.random() * 100);
-    if (hit > _fighter.getAgility()) {
-      console.log(`    ${this.getName()} make ${this.getDamage()} damage to ${_fighter.getName()}`);
-      _fighter.dealDamage(this.getDamage());
-    } else {
-      console.log(`    ${this.getName()} attack missed`);
+  class Fighter {
+    constructor (_fighter) {
+      privateData.set(this, {
+          name: _fighter.name,
+          damage: _fighter.damage,
+          hp: _fighter.hp,
+          agility:_fighter.agility,
+          wins: 0,
+          losses: 0
+      });
     }
-  }
 
-  logCombatHistory() {
-    console.log(`    Name: ${this.#name}, Wins: ${this.#wins}, Losses: ${this.#losses}`);
-  }
-
-  heal(_hp) {
-    this.#hp = this.#hp + _hp;
-    if (this.#hp > 100) {
-      this.#hp = 100;
+    getName () {
+      return privateData.get(this).name;
     }
-    console.log(`    Name: ${this.#name}, hp: ${this.#hp}`);
-  }
 
-  dealDamage (_damage) {
-    this.#hp = this.#hp - _damage;
-    if (this.#hp < 0) {
-      this.#hp = 0;
+    getDamage () {
+      return privateData.get(this).damage;
     }
-    console.log(`    ${this.#name} hp: ${this.#hp}`);
-  }
-  
-  addWin() {
-    this.#wins = this.#wins +1;
-  }
 
-  addLoss() {
-    this.#losses = this.#losses +1;
-  }
+    getHealth () {
+      return privateData.get(this).hp;
+    }
 
-  // hit (enemy, point) {
-  //   let damage = point * this.#damage;
-  //   enemy.setDamage(damage);
-  // }
-}
+    getAgility () {
+      return privateData.get(this).agility;
+    }
 
-// class ImprovedFighter extends Fighter{
-//   doubleHit (improvedFighter, point) {  	
-//   	super.hit(improvedFighter, point * 2);    
-//   } 
-// }
+    attack(_fighter) {
+      let hit = Math.round(Math.random() * hundred);
+      if (hit > _fighter.getAgility()) {
+        console.log(`    ${this.getName()} make ${this.getDamage()} damage to ${_fighter.getName()}`);
+        _fighter.dealDamage(this.getDamage());
+      } else {
+        console.log(`    ${this.getName()} attack missed`);
+      }
+    }
 
-// let fights = (fighter, improvedFighter, ...point) => {
-//   console.log(`\nTHE BATTLE BETWEEN ${fighter.getName()} vs ${improvedFighter.getName()}:`);  
-//   let i = 0;
-//   while ((fighter.getHealth() > 0) && (improvedFighter.getHealth() > 0)) {       	
-//     	console.log(`Atacking ${fighter.getName()}...`);  
-//     	fighter.hit(improvedFighter, point[i]);
-      
-//     if (improvedFighter.getHealth() > 0) {
-//     	console.log(`Atacking ${improvedFighter.getName()}...`);  
-//       improvedFighter.doubleHit(fighter, point[i]);
-//     }
+    logCombatHistory() {
+      console.log(`    Name: ${this.getName()}, Wins: ${privateData.get(this).wins}, \
+Losses: ${privateData.get(this).losses}`);
+    }
+
+    heal(_hp) {
+      let obj = privateData.get(this);
+      obj.hp = obj.hp + _hp;
+      if (obj.hp > hundred) {
+          obj.hp = hundred;
+      }
+     privateData.set(this,obj);
+      console.log(`    ${this.getName()} hp: ${this.getHealth()}`);
+    }
+
+    dealDamage (_damage) {
+      let obj = privateData.get(this);
+      obj.hp = obj.hp - _damage;
+      if (obj.hp < 0) {
+          obj.hp = 0;
+      }
+      privateData.set(this,obj);
+      console.log(`    ${this.getName()} hp: ${this.getHealth()}`);
+    }
     
-//     i++;
-//   }
-//   console.log(`GAME OVER!!!`);
-//   if (fighter.getHealth() <= 0) {
-//   	console.log(`WINNER is ***${improvedFighter.getName()}***!!!!!`);
-//   } else if (improvedFighter.getHealth() <= 0) {
-//   	console.log(`WINNER is ***${fighter.getName()}***!!!!!`);
-//   }  
-// }
-/*console.log(`\nPRESENT FIGHTERS:`);  
-let fighter = new Fighter('Bob', 1, 100);
-let improvedFighter = new ImprovedFighter('Sam', 1, 100);
-fights(fighter, improvedFighter, 25, 13, 45,50);
+    addWin() {
+      let obj = privateData.get(this);
+      obj.wins = obj.wins + 1;    
+      privateData.set(this,obj);
+    }
 
+    addLoss() {
+      let obj = privateData.get(this);
+      obj.losses = obj.losses + 1;    
+      privateData.set(this,obj);
+    }
 
-console.log(`\nPRESENT FIGHTERS:`); 
-fighter = new Fighter(undefined, 3, 100);
-improvedFighter = new ImprovedFighter('Sam', 1, 100);
-fights(fighter, improvedFighter, 25, 13, 45,50);*/
-
-console.log(`\nPRESENT FIGHTERS:`);
-const myFighter = new Fighter('John', 20, 100, 25);
-// console.log(myFighter);
-// console.log(myFighter.name);
-// console.log(myFighter.getName());
-// console.log(myFighter.getDamage());
-// console.log(myFighter.getHealth());
-// console.log(myFighter.getAgility());
-// console.log(myFighter.logCombatHistory());
-// myFighter.heal(10);
-// myFighter.dealDamage(15);
-// myFighter.addWin();
-// myFighter.addLoss();
-// console.log(myFighter.logCombatHistory());
-//debugger;
-
-console.log(`\nPRESENT FIGHTERS:`);
-const myFighter2 = new Fighter('Bob', 30, 100, 35);
-// console.log(myFighter2);
-// console.log(myFighter2.name);
-// console.log(myFighter2.getName());
-// console.log(myFighter2.getDamage());
-// console.log(myFighter2.getHealth());
-// console.log(myFighter2.getAgility());
-// console.log(myFighter2.logCombatHistory());
+    presentFighter() {
+      console.log(`    ${privateData.get(this).name} : 
+      damage - ${privateData.get(this).damage}, 
+      hp - ${privateData.get(this).hp}, 
+      agility - ${privateData.get(this).agility}`);
+    }
+  }
+  return Fighter;
+})();
 
 function battle (fighter, fighterEnemy) {
-  if (fighter.getHealth() === 0)  {
+  console.log(`\nPRESENT FIGHTERS:`);
+  fighter.presentFighter();
+  fighterEnemy.presentFighter();
+
+  if (fighter.getHealth() === 0) {
     console.log(`    ${fighter.getName()} is dead and can't fight.`);
     return;
-  }else if (fighterEnemy.getHealth() === 0)  {
+  }else if (fighterEnemy.getHealth() === 0) {
      console.log(`    ${fighterEnemy.getName()} is dead and can't fight.`);
     return;
   }
 
   console.log(`\nTHE BATTLE BETWEEN ${fighter.getName()} vs ${fighterEnemy.getName()}:`);
-  let i = 0;
-  while ((fighter.getHealth() > 0) && (fighterEnemy.getHealth() > 0)) {
+
+  while (fighter.getHealth() > 0 && fighterEnemy.getHealth() > 0) {
     console.log(`Atacking ${fighter.getName()}...`);
     fighter.attack(fighterEnemy);
 
@@ -157,8 +111,6 @@ function battle (fighter, fighterEnemy) {
       console.log(`Atacking ${fighterEnemy.getName()}...`);
       fighterEnemy.attack(fighter);
     }
-
-    i++;
   }
   console.log(`GAME OVER!!!`);
   if (fighter.getHealth() <= 0) {
@@ -170,10 +122,17 @@ function battle (fighter, fighterEnemy) {
     fighter.addWin();
     fighterEnemy.addLoss();
   }
+  fighter.logCombatHistory();
+  fighterEnemy.logCombatHistory();
   return;
 }
 
-battle(myFighter, myFighter2);
-console.log(myFighter.logCombatHistory());
-console.log(myFighter2.logCombatHistory());
-battle(myFighter, myFighter2);
+const twenty = 20;
+const twentyFive = 25;
+const thirty = 30;
+const thirtyFive = 35;
+
+const fighterJohn = new Fighter({name: 'John', damage: twenty, hp: hundred, agility: twentyFive});
+const fighterBob = new Fighter({name: 'Bob', damage: thirty, hp: hundred, agility: thirtyFive});
+battle(fighterJohn, fighterBob);
+battle(fighterJohn, fighterBob);
