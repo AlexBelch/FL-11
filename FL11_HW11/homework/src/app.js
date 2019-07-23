3e9d7ce858754aa5802e0d7250d19a969f0c061a
Oleksandr Belchitskyi <oleksandr.belchitskyi@gmail.com>
2019-07-23 19:34:47 +0300

    18. Task #11. Change files according to the             task description #11
                Create application for task #11
    	    add drag and drop, fix checked after edit/drag_drop
                    FL11_HW11/homework/index.html
    		FL11_HW11/homework/src/app.js
    		FL11_HW11/homework/assets/styles.css.

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,6 +1,67 @@
 let rootNode = document.getElementById('root');
 const maxItems = 10;
 let id = 1;
+let dragSrcEl = null;
+let checkedEdit = [];
+
+function handleDragStart(e) {
+    dragSrcEl = this;
+    e.dataTransfer.effectAlloved = 'move';
+    e.dataTransfer.setData('text/html', this.outerHTML);
+
+    this.classList.add('dragElem');
+}
+
+function handleDragOver(e) {
+    if (e.preventDefault) {
+        e.preventDefault();
+    }
+    this.classList.add('over');
+
+    e.dataTransfer.dropEffect = 'move';
+    return false;
+}
+
+function handleDragLeave() {
+    this.classList.remove('over');
+}
+
+function handleDrop(e) {  
+    if (e.stopPropagation) {
+        e.stopPropagation();
+    }
+
+    if (dragSrcEl !== this) {
+        this.parentNode.removeChild(dragSrcEl);
+        let dropHTML = e.dataTransfer.getData('text/html');
+        this.insertAdjacentHTML('beforebegin',dropHTML);
+        let dropElem = this.previousSibling;
+        if (dropElem.children[0].disabled) {
+        dropElem.children[0].checked = true;
+        }
+        addDnDHandlers(dropElem);
+        const editButton = 2;
+        const delButton = 3; 
+        document.getElementById(dropElem.children[0].id).addEventListener('click', checked);
+        document.getElementById(dropElem.children[editButton].id).addEventListener('click', editTask);
+        document.getElementById(dropElem.children[delButton].id).addEventListener('click', delTask);
+    }
+    this.classList.remove('over');
+    return false;
+}
+
+function handleDragEnd() {
+    this.classList.remove('over');
+    this.classList.remove('dragElem');
+}
+
+function addDnDHandlers(elem) {
+    elem.addEventListener('dragstart', handleDragStart, false);
+    elem.addEventListener('dragover', handleDragOver, false);
+    elem.addEventListener('dragleave', handleDragLeave, false);
+    elem.addEventListener('drop', handleDrop, false);
+    elem.addEventListener('dragend', handleDragEnd, false);
+}
 
 function changeInput() {
     const name = document.getElementById('list-add-name').value;
@@ -13,6 +74,7 @@ function changeInput() {
 
 function checked(event) {
     event.target.disabled='disabled';
+    checkedEdit[event.target.id] = true;
 }
 
 function addTask() {
@@ -22,6 +84,7 @@ function addTask() {
 
     let elemDiv = document.createElement('div');
     elemDiv.classList.add('list-item-tasks');
+    elemDiv.draggable = 'true';
 
     let checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
@@ -55,6 +118,7 @@ function addTask() {
     document.getElementById(checkbox.id).addEventListener('click', checked);
     document.getElementById(btnDelete.id).addEventListener('click', delTask);
     document.getElementById(btnEdit.id).addEventListener('click', editTask);
+    addDnDHandlers(elemDiv);
     id = id + 1;
 }
 
@@ -65,6 +129,7 @@ function editTask(event) {
     let inputName = document.createElement('input');
     inputName.type = 'text';
     inputName.name = 'text';
+    inputName.id = event.target.parentNode.parentNode.children[0].id;
     inputName.value = valueName;
 
     let btnSave = document.createElement('button');	
@@ -75,6 +140,7 @@ function editTask(event) {
 
     item.appendChild(inputName);
     item.appendChild(btnSave);
+
     listsNode.replaceChild(item, event.target.parentNode.parentNode);
 
     document.getElementById(btnSave.id).addEventListener('click', saveTask);
@@ -86,11 +152,16 @@ function saveTask(event) {
 
     let elemDiv = document.createElement('div');
     elemDiv.classList.add('list-item-tasks');
+    elemDiv.draggable = 'true';
 
     let checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.name = 'checkbox';
-    checkbox.id = id;  
+    checkbox.id = id;
+    if (checkedEdit[event.target.parentNode.parentNode.children[0].id]) {
+        checkbox.checked = true;
+        checkbox.disabled = 'disabled';
+    }
     let div = document.createElement('div');
     div.innerHTML = event.target.parentNode.parentNode.children[0].value;
     let btnEdit = document.createElement('button');
@@ -114,6 +185,7 @@ function saveTask(event) {
     document.getElementById(checkbox.id).addEventListener('click', checked);
     document.getElementById(btnDelete.id).addEventListener('click', delTask);
     document.getElementById(btnEdit.id).addEventListener('click', editTask);
+    addDnDHandlers(elemDiv);
     id = id + 1;
 }
 
